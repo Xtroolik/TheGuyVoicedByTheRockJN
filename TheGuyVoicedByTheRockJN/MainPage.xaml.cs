fcd9a760d2dd49b473264bbb940f801b39c6a853
Xtroolik <xtroolik@gmail.com>
2024-09-16 09:30:50 +0200

    DiceRoller

--- TheGuyVoicedByTheRockJN/MainPage.xaml.cs
+++ TheGuyVoicedByTheRockJN/MainPage.xaml.cs
@@ -10,10 +10,30 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-                Random r = new Random();
-
+            Random r = new Random();
+            if (d4.IsChecked == true)
+            {
+                int roll = r.Next(1, 5);
+                rollValue.Text = "Rolled the number: " + roll.ToString();
+            }
+            else if (d6.IsChecked == true)
+            {
                 int roll = r.Next(1, 7);
-                CounterBtn.Text = "Rolled the number: " + roll.ToString();
+                rollValue.Text = "Rolled the number: " + roll.ToString();
+            }
+            else if (d10.IsChecked == true)
+            {
+                int roll = r.Next(1,11);
+                rollValue.Text = "Rolled the number: " + roll.ToString();
+            }
+            else if (d12.IsChecked == true)
+            {
+                int roll = r.Next(1,13);
+                rollValue.Text = "Rolled the number: " + roll.ToString();
+            }
+
+
+
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
