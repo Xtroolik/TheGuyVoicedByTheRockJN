4be96f587288a3995f3b2e7d5fbb23b8c75126d0
Xtroolik <xtroolik@gmail.com>
2024-09-16 08:53:14 +0200

    Dodaj pliki projektów.

---
+++ TheGuyVoicedByTheRockJN/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TheGuyVoicedByTheRockJN
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TheGuyVoicedByTheRockJN/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace TheGuyVoicedByTheRockJN
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+                Random r = new Random();
+
+                int roll = r.Next(1, 7);
+                CounterBtn.Text = "Rolled the number: " + roll.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
